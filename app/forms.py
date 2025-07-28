8ad04c945ebbf63ab1bfbb571391fadbc1ac495e
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-07-28 21:30:13 +0500

    third commit

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Avtosalon, Brand, Cars
+import re
+from django.core.exceptions import ValidationError
 
 class BrandForm(forms.ModelForm):
     class Meta:
@@ -16,22 +18,52 @@ class BrandForm(forms.ModelForm):
             'title':'name',
             'context':'text',
             'brand_salon':'Avtosalon'
+        }    
+
+
+class AvtosalonForm(forms.ModelForm):
+    class Meta:
+        model = Avtosalon
+        fields = ['title','context','email','phone','address','image']
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'context':forms.Textarea(attrs={'class':'form-control','rows':5}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'phone':forms.TextInput(attrs={'class':'form-control'}),
+            'address':forms.TextInput(attrs={'class':'form-control'}),
+            'image':forms.ClearableFileInput(attrs={'class':'form-control-file'})
+
         }
         
+        label = {
+            'title':'name',
+            'context':'text',
+            'email':'enter email',
+            'phone':'phone number',
+            'image':'upload image',
+        }
 
-class AvtosalonForm(forms.Form):
-    title = forms.CharField(max_length = 40,
-                           widget=forms.TextInput(attrs={'class':'form-control'}),
-                           label='name')
-    context = forms.CharField(label='text',required=False,
-                              widget=forms.Textarea(
-                                  attrs={'class':'form-control','rows':5}))
-    email =forms.EmailField(required=False,widget=forms.EmailInput(attrs={'class':'form-control'}))
-    phone = forms.CharField(required=False,widget=forms.TextInput(attrs={'class':'form-control'}))
-    address = forms.CharField(required=False,widget=forms.TextInput(attrs={'class':'form-control'}))
-    image = forms.ImageField(required=False,
-                             label='upload image')
+    def clean_title(self):
+        title = self.cleaned_data['title']
 
+        if re.match(r'\d',title):
+            raise ValidationError('Title da raqam bolmasin!')
+        return title
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if re.match(r'[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+',email):
+            raise ValidationError('Togri emalil kiriting!')
+        return email
+    
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+
+        if re.match(r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$',phone):
+            raise ValidationError('togri telefon raqam kiriting!')        
+        return phone
 
 
 class CarForm(forms.Form):
